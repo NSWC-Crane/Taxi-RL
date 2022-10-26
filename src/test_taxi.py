9f303116e6d143d17f6dd306464e95d495b3a909
Dave <davemers@iu.edu>
2022-10-25 22:08:05 -0400

    initial commit

---
+++ src/test_taxi.py
@@ -0,0 +1,54 @@
+import numpy as np
+import gym
+import pygame
+import random
+
+# create Taxi environment
+env = gym.make('Taxi-v3')
+env.reset()
+env.step(1)
+env.render(mode='human')
+
+# get the basic size fo the action/state space
+state_size = env.observation_space.n
+action_size = env.action_space.n
+
+# testing variables
+num_episodes = 2000
+max_steps = 100  # per episode
+
+# load in teh saved qtable from the training
+qtable = np.load("../data/taxi_qtable.npy")
+
+q_est = np.zeros((state_size, action_size))
+
+# Challenge:
+#  1. we don't know the state numbering
+#  2. we do know the actions, but not the correct order
+#
+
+# watch trained agent
+for idx in range(num_episodes):
+
+    state = env.reset()
+    done = False
+    rewards = 0
+
+    print("Episode {}".format(idx))
+
+    for s in range(max_steps):
+
+        action = np.argmax(qtable[state,:])
+        new_state, reward, done, info = env.step(action)
+        rewards += reward
+        # env.render(mode='human')
+        state = new_state
+
+        if done == True:
+            print("  Step {}".format(s+1))
+            print("  Score: {}".format(rewards))
+            break
+
+
+env.close()
+
